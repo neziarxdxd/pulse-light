38e3f4b5e729a52fe2f19231bb44e3cd49f5e9ba
zenraiching <rickdavidsangalang@gmail.com>
2021-01-15 00:32:36 +0800

    added installer

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:flutter_better_camera/camera.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:wakelock/wakelock.dart';
 import 'chart.dart';
