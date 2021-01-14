e9db8d9107b9483115ec3ad5ac771d98d906351c
zenraiching <rickdavidsangalang@gmail.com>
2021-01-15 00:04:15 +0800

    added installer

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:testapp/homePage.dart';
 import 'package:testapp/main.dart';
 
 void main() {
