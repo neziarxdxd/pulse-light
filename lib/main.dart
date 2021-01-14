589e66cde89c05ef1f555081eab745e7b48f1e33
zenraiching <rickdavidsangalang@gmail.com>
2021-01-14 22:56:10 +0800

    changes the name

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,196 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:wakelock/wakelock.dart';
+import 'chart.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  HomePageView createState() {
+    return HomePageView();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class HomePageView extends State<MyApp> {
+  bool _toggled = false;
+  bool _processing = false;
+  List<SensorValue> _data = [];
+  CameraController _controller;
+  double _alpha = 0.3;
+  int _bpm = 0;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  _toggle() {
+    _initController().then((onValue) {
+      Wakelock.enable();
+      setState(() {
+        _toggled = true;
+        _processing = false;
+      });
+      _updateBPM();
     });
   }
 
+  _untoggle() {
+    _disposeController();
+    Wakelock.disable();
+    setState(() {
+      _toggled = false;
+      _processing = false;
+    });
+  }
+
+  Future<void> _initController() async {
+    try {
+      List _cameras = await availableCameras();
+      _controller = CameraController(_cameras.first, ResolutionPreset.low);
+      await _controller.initialize();
+      Future.delayed(Duration(milliseconds: 500)).then((onValue) {
+        _controller.flash(true);
+      });
+      _controller.startImageStream((CameraImage image) {
+        if (!_processing) {
+          setState(() {
+            _processing = true;
+          });
+          _scanImage(image);
+        }
+      });
+    } catch (Exception) {
+      print(Exception);
+    }
+  }
+
+  _updateBPM() async {
+    List<SensorValue> _values;
+    double _avg;
+    int _n;
+    double _m;
+    double _threshold;
+    double _bpm;
+    int _counter;
+    int _previous;
+    while (_toggled) {
+      _values = List.from(_data);
+      _avg = 0;
+      _n = _values.length;
+      _m = 0;
+      _values.forEach((SensorValue value) {
+        _avg += value.value / _n;
+        if (value.value > _m) _m = value.value;
+      });
+      _threshold = (_m + _avg) / 2;
+      _bpm = 0;
+      _counter = 0;
+      _previous = 0;
+      for (int i = 1; i < _n; i++) {
+        if (_values[i - 1].value < _threshold &&
+            _values[i].value > _threshold) {
+          if (_previous != 0) {
+            _counter++;
+            _bpm +=
+                60000 / (_values[i].time.millisecondsSinceEpoch - _previous);
+          }
+          _previous = _values[i].time.millisecondsSinceEpoch;
+        }
+      }
+      if (_counter > 0) {
+        _bpm = _bpm / _counter;
+        setState(() {
+          _bpm = (1 - _alpha) * _bpm + _alpha * _bpm;
+        });
+      }
+      await Future.delayed(Duration(milliseconds: (1000 * 50 / 30).round()));
+    }
+  }
+
+  _scanImage(CameraImage image) {
+    double _avg =
+        image.planes.first.bytes.reduce((value, element) => value + element) /
+            image.planes.first.bytes.length;
+    if (_data.length >= 50) {
+      _data.removeAt(0);
+    }
+    setState(() {
+      _data.add(SensorValue(DateTime.now(), _avg));
+    });
+    Future.delayed(Duration(milliseconds: 1000 ~/ 30)).then((onValue) {
+      setState(() {
+        _processing = false;
+      });
+    });
+  }
+
+  _disposeController() {
+    _controller.dispose();
+    _controller = null;
+  }
+
+  @override
+  void dispose() {
+    _disposeController();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: Colors.white,
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Center(
+                      child: _controller == null
+                          ? Container()
+                          : CameraPreview(_controller),
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        (_bpm > 30 && _bpm < 150
+                            ? _bpm.round().toString()
+                            : "--"),
+                        style: TextStyle(
+                            fontSize: 32, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: Center(
+                child: IconButton(
+                  icon: Icon(_toggled ? Icons.favorite : Icons.favorite_border),
+                  color: Colors.red,
+                  iconSize: 128,
+                  onPressed: () {
+                    if (_toggled) {
+                      _untoggle();
+                    } else {
+                      _toggle();
+                    }
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(18),
+                    ),
+                    color: Colors.black),
+                child: Chart(_data),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
